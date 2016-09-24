c919c006a881f9f7b600d5377fd4decc080b4455
KajWestin <kaj.westin@live.se>
2016-09-24 17:03:49 +0200

    Skapar strukturen för lösningen

---
+++ dev_3/Core/Logic/IVehicle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Logic
+{
+    interface IVehicle
+    {
+        int GetSpeed();
+        void IncreaseSpeed(int amount);
+        void DecreaseSpeed(int amount);
+    }
+}
